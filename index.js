104d7e60b8dda3f7e37ac38b94194cc101893347
Guruprasad Kamath <48196632+GuruMLAI@users.noreply.github.com>
2021-07-06 12:49:14 +0200

    Input Sequence Based Logic

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-// $(".btn").click(function(event){
-//     console.log(event.currentTarget.innerHTML);
-//     // console.log(event);
-// })
-
-var onScreenNum = 0;
-var offScreenNum = 0;
-var currentOp = "";
-
-// var num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var op_list = ['<i class="fas fa-plus" aria-hidden="true"></i>',
             '<i class="fas fa-minus" aria-hidden="true"></i>',
             '<i class="fas fa-times times-btn" aria-hidden="true"></i>',
-            '<i class="fas fa-divide" aria-hidden="true"></i>',
-            "="]
-// var cancel = "C";
-// var result = "=";
+            '<i class="fas fa-divide" aria-hidden="true"></i>']
 
+var num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var currentQueue = [];
+var prevButtonClick = ""
+;
 function displayNumOnScreen(num){
     $("h1").text(num);
 }
 
 function clearDisplay(){
-    // offScreenNum = onScreenNum;
-    // onScreenNum = 0;
     displayNumOnScreen(0);
 }
 
-function calc(num1, num2, op){
+function calc(num1, op, num2){
     if (op_list.indexOf(op) === 0){
         var x = num1 + num2;
         return Math.round((x + Number.EPSILON) * 100) / 100;
@@ -46,52 +36,92 @@ function calc(num1, num2, op){
         var x = num1/num2;
         return Math.round((x + Number.EPSILON) * 100) / 100;
     }
-    if (op_list.indexOf(op) === 4){
-        var x = num2;
-        return Math.round((x + Number.EPSILON) * 100) / 100;
-    }
     else {
         return num1 + num2;
     }
 }
 
+function eval(queue){
+    // while (op_list.includes(queue[queue.length - 1])){
+    //     queue = queue.splice(0,queue.length-1);
+    // } 
+    while (queue.length > 1) {
+        queue = [calc(queue[0],queue[1],queue[2])].concat(queue.splice(3,queue.length))
+    }
+    return queue[0];
+}
+
 $(".num-btn").click(function(event){
-    if (currentOp === "="){
-        offScreenNum = 0;
+    if (op_list.includes(prevButtonClick) || prevButtonClick === "="){
         var entry = parseInt(event.currentTarget.innerHTML);
-        onScreenNum = onScreenNum*10 + entry;
-        displayNumOnScreen(onScreenNum);
+        displayNumOnScreen(entry);
+        prevButtonClick = event.currentTarget.innerHTML;
     }
     else {
         var entry = parseInt(event.currentTarget.innerHTML);
+        var onScreenNum = parseInt($("h1").text());
         onScreenNum = onScreenNum*10 + entry;
         displayNumOnScreen(onScreenNum);
+        prevButtonClick = event.currentTarget.innerHTML;
     }
-    
-    // console.log("onScreenNum :" + onScreenNum);
-    // console.log("offScreenNum :" + offScreenNum);
-    // console.log("currentOp :" + currentOp);
 })
 
-$(".operator").click(function(event){
-    var rslt = calc(offScreenNum, onScreenNum, currentOp);
-    offScreenNum = rslt;
-    onScreenNum = 0;
-    displayNumOnScreen(rslt);
-    currentOp =  event.currentTarget.innerHTML;
-})
 
 $(".clear").click(function(event){
-    onScreenNum = 0;
-    offScreenNum = 0;
-    currentOp = "";
-    displayNumOnScreen(onScreenNum);
+    currentQueue = [];
+    displayNumOnScreen(0);
+    prevButtonClick = "";
+})
+
+
+$(".operator").click(function(event){
+    console.log(num_list.includes(prevButtonClick));
+    if (prevButtonClick === ""){
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else if (num_list.includes(prevButtonClick)){
+        var onScreenNum = parseInt($("h1").text());
+        currentQueue = currentQueue.concat([onScreenNum]);
+        var rslt = eval(currentQueue);
+        currentQueue = [rslt, event.currentTarget.innerHTML];
+        displayNumOnScreen(rslt);
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else if (op_list.includes(prevButtonClick)){
+        currentQueue = currentQueue.splice(0, currentQueue.length - 1);
+        currentQueue = currentQueue.concat([event.currentTarget.innerHTML]);
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else if (prevButtonClick === "=") {
+        currentQueue = currentQueue.concat([event.currentTarget.innerHTML]);
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else {
+        console.log("After operator: " + "others");
+    }
+    
 })
 
 $(".result").click(function(event){
-    var rslt = calc(offScreenNum, onScreenNum, currentOp);
-    offScreenNum = 0;
-    onScreenNum = rslt;
-    currentOp = "=";
-    displayNumOnScreen(onScreenNum);
+
+    if (prevButtonClick === ""){
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else
+    if (num_list.includes(prevButtonClick)){
+        var onScreenNum = parseInt($("h1").text());
+        currentQueue = currentQueue.concat([onScreenNum]);
+        var rslt = eval(currentQueue);
+        currentQueue = [rslt];
+        displayNumOnScreen(rslt);
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else
+    if (op_list.includes(prevButtonClick)){
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
+    else
+    if (prevButtonClick === "=") {
+        prevButtonClick = event.currentTarget.innerHTML;
+    }
 })
